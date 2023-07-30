9665bc21938258f14ab4bc6d72c4b7903be72f71
SakthivelKrishnamurthi <sakthivelkr20@gmail.com>
2023-07-30 11:59:32 +0530

    first commit

---
+++ Python/check_db.py
@@ -0,0 +1,46 @@
+from pickle import FALSE
+from flask import Blueprint , render_template,request
+import psycopg2
+import pyautogui
+check_db = Blueprint("check_db" , __name__ , static_folder="static" , template_folder="templates")
+
+@check_db.route("/details" ,methods=['GET','POST'])
+@check_db.route("/",methods=['GET','POST'])
+def details():
+    
+    # Database connection configuration
+    host = "localhost"
+    database = "patient_details"
+    user = "postgres"
+    password = "Saravana$$$14"  # Replace with your actual password
+
+    if request.method == 'POST':
+        # Retrieve form data
+        email = request.form['email']
+        psword = request.form['password']
+
+        # Connect to the PostgreSQL database
+        connection = psycopg2.connect(host=host, database=database, user=user, password=password)
+        cursor = connection.cursor()
+
+        # Check presence of form data in table
+        select_query = "SELECT email FROM users WHERE email=%s AND password=crypt(%s,password)"
+        cursor.execute(select_query,(email,psword))
+
+        selected_emails = cursor.fetchall()
+    
+        # Close the database connection
+        cursor.close()
+        connection.close()
+
+    
+        #Code for redirecting pages 
+        if selected_emails :
+            # Code for entering into details page
+            return render_template("Pages/details.html") 
+        else:
+            # Code for indicating incorrect username or password
+            pyautogui.alert("Invalid Username or Password")
+            return render_template("index.html")
+
+    
